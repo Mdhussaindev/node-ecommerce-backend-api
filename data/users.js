61e9b23c64b4205096ca82f088e007a1408514cd
Mdhussaindev <mdhussain.dev00@gmail.com>
2026-09-19 14:16:29 +0500

    update

---
+++ data/users.js
@@ -0,0 +1,22 @@
+const users = [
+  {
+    id: 1,
+    name: "Ali Raza",
+    email: "ali@example.com",
+    age: 22
+  },
+  {
+    id: 2,
+    name: "Sara Ahmed",
+    email: "sara@example.com",
+    age: 24
+  },
+  {
+    id: 3,
+    name: "Hamza Khan",
+    email: "hamza@example.com",
+    age: 21
+  }
+];
+
+module.exports = users;
